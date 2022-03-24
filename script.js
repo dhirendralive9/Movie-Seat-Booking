a07862603fce0793bdb8cef2c2477e9ba5b9324e
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-03-24 13:00:24 +0530

    added retrive data from local storage.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var count = document.getElementById('count');
 var total = document.getElementById('total');
 var movieSelect = document.getElementById('movie');
 // selecting all the ids and classes required.
+populateUI();
 
 var ticketPrice = +movieSelect.value;  //Converting to integer
 
 // Save Selected movie index
+
 
 setMovieData = (movieIndex,moviePrice) =>{
     localStorage.setItem('selectedMovieIndex',movieIndex);
@@ -19,7 +21,7 @@ setMovieData = (movieIndex,moviePrice) =>{
 updateSelectedCount = () =>{
    var selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-   const seatsIndex = [...selectedSeats].map((seat)=>
+   const seatsIndex = [...selectedSeats].map(seat =>
        [...seats].indexOf(seat));
 
    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
@@ -28,7 +30,30 @@ updateSelectedCount = () =>{
    count.innerText = selectedSeatsCount;
    total.innerText = selectedSeatsCount*ticketPrice;
    
+   setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
+
+// get Data from Local Storage nad populate UI
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('selected');
+        }
+      });
+    }
+  
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  
+    if (selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+    }
+  }
+  
+
 // /movie selector event listener 
 
 movieSelect.addEventListener('change',(e)=>{
@@ -50,3 +75,10 @@ container.addEventListener('click',(e)=>{
      updateSelectedCount();
 
 });  
+
+
+//initial retrive local dta  
+
+updateSelectedCount();
+populateUI();
+
